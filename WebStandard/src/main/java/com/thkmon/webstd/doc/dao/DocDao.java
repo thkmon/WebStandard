0c1b27c5e1aea202088a5debe7593e9b5aff9a0e
thkmon <thkmon@gmail.com>
2019-05-15 02:55:03 +0900

    190515. 인서트 기초 구현.

--- WebStandard/src/main/java/com/thkmon/webstd/doc/dao/DocDao.java
+++ WebStandard/src/main/java/com/thkmon/webstd/doc/dao/DocDao.java
@@ -1,10 +1,9 @@
 package com.thkmon.webstd.doc.dao;
 
-import java.util.HashMap;
-
 import org.springframework.stereotype.Repository;
 
 import com.thkmon.webstd.common.dao.AbstractDao;
+import com.thkmon.webstd.doc.data.Doc;
 
 @Repository("docDao")
 public class DocDao extends AbstractDao {
@@ -12,10 +11,13 @@ public class DocDao extends AbstractDao {
 	public boolean addDoc() throws Exception {
         // 여기서 doc.add 란, 네임스페이스가 doc 인 *-mapper.xml 내부의, id 가 add 인 쿼리를 의미한다.
         // 즉, doc-mapper.xml 파일에 담긴 쿼리를 뜻한다.
-//		HashMap<String, String> params = null;
-//		Object resultObj = insert("doc.add", params);
 		
-		System.out.println("addDoc");
+		// System.out.println("doc.addDoc");
+		
+		Doc doc = new Doc();
+		doc.setDocId("111");
+		doc.setDocContent("222");
+		insert("doc.addDoc", doc);
 		return true;
     }
 }
---
+++ WebStandard/src/main/java/com/thkmon/webstd/doc/data/Doc.java
@@ -0,0 +1,68 @@
+package com.thkmon.webstd.doc.data;
+
+public class Doc {
+
+	/**
+	 * 문서아이디
+		문서제목
+		문서내용
+		등록자아이디
+		등록자이름
+		검색엔진 등록여부
+	 */
+	
+	private String docId = "";
+	private String docTitle = "";
+	private String docContent = "";
+	private String regUserId = "";
+	private String regUserName = "";
+	private String inSearchEngine = "";
+	
+	public String getDocId() {
+		return docId;
+	}
+	
+	public void setDocId(String docId) {
+		this.docId = docId;
+	}
+	
+	public String getDocTitle() {
+		return docTitle;
+	}
+	
+	public void setDocTitle(String docTitle) {
+		this.docTitle = docTitle;
+	}
+	
+	public String getDocContent() {
+		return docContent;
+	}
+	
+	public void setDocContent(String docContent) {
+		this.docContent = docContent;
+	}
+	
+	public String getRegUserId() {
+		return regUserId;
+	}
+	
+	public void setRegUserId(String regUserId) {
+		this.regUserId = regUserId;
+	}
+	
+	public String getRegUserName() {
+		return regUserName;
+	}
+	
+	public void setRegUserName(String regUserName) {
+		this.regUserName = regUserName;
+	}
+	
+	public String getInSearchEngine() {
+		return inSearchEngine;
+	}
+	
+	public void setInSearchEngine(String inSearchEngine) {
+		this.inSearchEngine = inSearchEngine;
+	}
+}
